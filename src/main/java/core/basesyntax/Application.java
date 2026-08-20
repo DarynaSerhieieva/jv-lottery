c2e21cf73b5db5dbfad12d4c3ef81ec8394b65ed
DarynaSerhieieva <darynaserhieieva@gmail.com>
2026-08-20 12:16:16 +0200

    Added small fixes suggested by the AI mentor.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,12 +2,13 @@ package core.basesyntax;
 
 public class Application {
     private static Lottery lottery = new Lottery();
+    private static final int NUMBER_OF_BALLS = 3;
 
     public static void main(String[] args) {
-        Ball[] balls = new Ball[3];
-        for (int i = 0; i < 3; i++) {
+        Ball[] balls = new Ball[NUMBER_OF_BALLS];
+        for (int i = 0; i < NUMBER_OF_BALLS; i++) {
             balls[i] = lottery.getRandomBall();
-            System.out.println(balls[i].toString());
+            System.out.println(balls[i]);
         }
     }
 }
